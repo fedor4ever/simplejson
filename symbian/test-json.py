72b3e5107b5aaca647935259fc1749e1a83c6ddc
Fedor Strizhnev <fedor_qd@mail.ru>
2016-11-16 23:03:05 +0300

    Add test-json.py

---
+++ symbian/test-json.py
@@ -0,0 +1,8 @@
+import sys, subprocess
+#raise SystemExit(
+   #subprocess.call([sys.executable,
+   # Turn on deprecation warnings
+       #   '-Wd',
+     #     'simplejson/tests/__init__.py']))
+
+execfile('c:/resource/Python25/simplejson/tests/__init__.py')
